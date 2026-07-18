34565ecc4ecdd040ebfb9041ff90b72255fa6917
soundaryaov1530 <soundaryaov16@gmail.com>
2026-07-18 15:42:02 +0530

    Update prac.spec.js

--- tests/prac.spec.js
+++ tests/prac.spec.js
@@ -2,12 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('Login', async ({ page }) => {
 
-    await page.goto('https://practicetestautomation.com/practice-test-login/')
-    await page.locator('//input[@name="username"]').fill('student')
-    await page.locator('//input[@name="password"]').fill('Password123')
-    await page.locator('//button[@id="submit"]').click()
-    await page.getByRole('Log out').click()
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.locator('//input[@name="username"]').fill('student');
+    await page.locator('//input[@name="password"]').fill('Password123');
+    await page.locator('//button[@id="submit"]').click();
+
+    await page.getByRole('link', { name: 'Log out' }).click();
+
     await page.waitForTimeout(5000);
+
     // Feature branch practice
-})  
-// npx playwright test tests/prac.spec.js --project=chromium --headed
+});
